f2350e2f06244867c09529bf75d28bfcb4e16acd
Amansaxena6 <aman16nov.as@gmail.com>
2024-09-22 23:45:59 +0530

    added new changes

--- Backend/home/models.py
+++ Backend/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class SpaceData(models.Model):
+    name = models.CharField(max_length=30)
+    logo = models.ImageField(upload_to="static/image/")
+    title = models.CharField(max_length=50)
+    message = models.TextField(max_length=200)
+    quest1 = models.CharField(max_length=40)
+    quest2 = models.CharField(max_length=40)
+    quest3 = models.CharField(max_length=40)
+    starRating = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
--- Backend/home/views.py
+++ Backend/home/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import SpaceData
+from .serializers import SpaceSerializer
+from rest_framework.response import Response
 
-
+@api_view(['GET'])
+def getSpaceData(request):
+    spaces = SpaceData.objects.all()
+    print(spaces)
+    serializer = SpaceSerializer(spaces, many=True)
+    print('getting response')
+    return Response({
+        'Payload' : serializer.data
+    })
+    
+@api_view(['POST'])
+def postSpaceData(request):
+    data = request.data
+    serializer = SpaceSerializer(data = data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            'data' : serializer.data,
+            'message' : 'Data has been Saved Successfully'
+        })
+    else:
+        return Response(serializer.errors, status=400)
