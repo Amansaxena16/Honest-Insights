e22ba971b10924b7c154f47951e3e75f835d0eb9
Amansaxena16 <aman16nov.as@gmail.com>
2024-10-10 02:36:42 +0530

    added new file

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -79,12 +79,23 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'HonestInsights',  
+        'CLIENT': {
+            'host': 'mongodb://localhost:27017/',  # Ensure MongoDB is running locally
+        }
     }
 }
+             
 
 
 # Password validation
